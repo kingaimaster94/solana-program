4027d2f60f29176d2eb6c6574dd49bfec79d337d
kingaimaster94 <kingaimaster94@gmail.com>
2024-05-10 20:09:59 +0900

    basic

---
+++ dapp/basic/generate-keypair.ts
@@ -0,0 +1,20 @@
+import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const b = false;
+if (b) {
+    const keypair = Keypair.generate();
+    console.log('✅ generated keypair!');
+    console.log('The public key is: ', keypair.publicKey.toBase58());
+    console.log('The secret key is: ', keypair.secretKey);
+    console.log(`✅ Finished!`);
+} else {
+    const keypair = getKeypairFromEnvironment("SECRET_KEY");
+    // console.log('The public key is: ', keypair.publicKey.toBase58());
+    // console.log('The secret key is: ', keypair.secretKey);
+    console.log(
+        `✅ Finished! We've loaded our secret key securely, using an env file!`
+    );
+}
+
