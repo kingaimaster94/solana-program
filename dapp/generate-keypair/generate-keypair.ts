9f456eaa2def52dad19ff22f5b67efd706dbb540
kingaimaster94 <kingaimaster94@gmail.com>
2024-05-10 20:08:34 +0900

    basic

--- dapp/generate-keypair/generate-keypair.ts
+++
@@ -1,9 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-
-const keypair = Keypair.generate();
-console.log('✅ generated keypair!');
-
-console.log('The public key is: ', keypair.publicKey.toBase58());
-console.log('The secret key is: ', keypair.secretKey);
-console.log(`✅ Finished!`);
-
