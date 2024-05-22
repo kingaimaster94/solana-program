2732749cd086ec9329d36dfe71fbdcc94c491c2e
root <root@cex.com>
2024-05-22 10:36:15 +0300

    add

---
+++ dapp/basic/mint-tokens.ts
@@ -0,0 +1,34 @@
+import { mintTo } from "@solana/spl-token";
+import "dotenv/config";
+import {
+    getExplorerLink,
+    getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = 100; //= Math.pow(10, 2);
+
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+// Subtitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey(
+    "AYYytURTFpFhrMvYu87ht4HsY5kKddU3Z5nJ4FzKPdnQ"
+);
+
+// Substitute in your own, or a friend's token account address, based on the previous step.
+const recipientAssociatedTokenAccount = new PublicKey("FK4giYEXURYUs5m9vTE99YxyCCeCgJUMoT3Kt7CQZijs");
+
+const transactionSignature = await mintTo(
+    connection,
+    user,
+    tokenMintAccount,
+    recipientAssociatedTokenAccount,
+    user,
+    10 * MINOR_UNITS_PER_MAJOR_UNITS,
+);
+
+const link = getExplorerLink("transaction", transactionSignature, "devnet");
+
+console.log(`✅ Success! Mint Token Transaction: ${link}`);
